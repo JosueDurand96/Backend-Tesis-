c2be372ea01e5978c7904707d4518e8a77bcf48b
Josué Durand Pebe <josue.durand8@gmail.com>
2024-06-07 15:48:58 -0500

    create crud DeliveryPerson

--- controller/deliveryPersonController.js
+++ controller/deliveryPersonController.js
@@ -15,3 +15,45 @@ export const create = async(req, res) => {
         res.status(500).json({ error: "Internal server error."});
     }
 } 
+
+export const fetch = async(req, res) => {
+    try {
+        const deliveryPerson = await DeliveryPerson.find();
+        if(deliveryPerson.length === 0) {
+          return res.status(404).json({ error: "Apoderado no encontrado!" });
+        }
+        res.status(200).json({ data: deliveryPerson});
+    } catch(error){
+        res.status(500).json({ error: "Internal Server error." });
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const deliveryPersonExist = await DeliveryPerson.findOne({ _id: id});
+
+        if(!deliveryPersonExist) {
+          return res.status(404).json({ error: "Apoderado no encontrado!" });
+        }
+        const updateDeliveryPerson = await DeliveryPerson.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).json({ message: "Los datos han sido actualizado correctamente.", updateDeliveryPerson});
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error." });
+    }
+}
+
+export const deleteDeliveryPerson = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const deliveryPersonExist = await DeliveryPerson.findOne({ _id: id});
+        if(!deliveryPersonExist) {
+            return res.status(404).json({ error: "El apoderado no ha sido eliminado!" });
+        }
+        await DeliveryPerson.findByIdAndDelete(id);
+        res.status(201).json({ message: "Apoderado eliminado correctamente!"});
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error." });
+    }
+}
--- routes/deliveryPersonRoute.js
+++ routes/deliveryPersonRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { create } from "../controller/deliveryPersonController.js";
+import { create, fetch, update, deleteDeliveryPerson } from "../controller/deliveryPersonController.js";
 
 const routeDeliveryPerson = express.Router();
 
 routeDeliveryPerson.post("/create", create);
-
+routeDeliveryPerson.get("/getAll", fetch);
+routeDeliveryPerson.put("/update/:id", update);
+routeDeliveryPerson.delete("/delete/:id", deleteDeliveryPerson);
 export default routeDeliveryPerson;
