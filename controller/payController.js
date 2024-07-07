4b499f4562f93c84df0d887629ab29e5c09064dc
Josué Durand Pebe <josue.durand8@gmail.com>
2024-07-07 10:44:13 -0500

    payment

--- controller/payController.js
+++ controller/payController.js
@@ -2,9 +2,16 @@ import Pay from "../model/paymentModel.js"
 
 export const create = async(req, res) => {
     try {
-        const payData = new Pay(req.body);
-        const savedPay = await payData.save();
-        res.status(200).json({ mesagge: "Pago exitoso."});
+        const peliData = new Pay(req.body);
+        const { name } = peliData;
+        const peliExist = await Pay.findOne({ name });
+        if(peliExist){
+            const savedPeli = await peliData.save();
+            res.status(200).json(savedPeli);
+        } else {
+            return res.status(400).json( { message: "La pelicula ya existe!"} );
+        }
+        
     } catch (error) {
         res.status(500).json({ error: "Internal server error."});
     }
